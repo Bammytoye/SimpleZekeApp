b9ee57b650c72b61aab9decc93b8327689b02f09
bammytoye <bamigbalatoyese@gmail.com>
2024-08-20 14:04:03 +0100

    footer

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { TiStarOutline } from "react-icons/ti";
+import { IoLogoGooglePlaystore, IoLogoApple } from "react-icons/io5";
 
 const home = () => {
     return (
@@ -89,9 +90,8 @@ const home = () => {
             </div>
 
             {/* fourth component */}
-            <div>
-                <div className="px-32 py-10">
-                    <div>
+            <div className="px-32 py-10">
+                    <div className="flex flex-col justify-center items-center space-y-20">
                         <div className="flex justify-between items-center space-x-10">
                             <div className="w-[400px] space-y-5">
                                 <h3 className="text-4xl font-extrabold font-sans">
@@ -134,6 +134,103 @@ const home = () => {
                                 </div>
                             </div>
                         </div>
+
+                        <div className="text-center space-y-4">
+                            <h3 className="text-4xl font-extrabold font-sans">
+                                New to Cashout? We've got you covered 
+                            </h3>
+
+                            <p className="text-sm text-gray-700 pb-6">
+                                Save and earn interest on your dollar stable cryptocurrency with bundle vault - No risk involved 
+                            </p>
+                            
+                            <a href="#" className="border px-8 py-2 bg-blue-800 text-gray-300 hover:text-white text-sm rounded-md">
+                                Learn about Cashout
+                            </a>
+                        </div>
+
+                        <div>
+                            <Image src="/image/fourth(2).png" alt="" width="300" height="300" />
+                        </div>
+                    </div>
+            </div>
+
+            {/* fifth component */}
+            <div className="bg-gradient-to-r from-blue-200 via-purple-700 to-pink-800 shadow-2xl">
+                <div className="px-52 py-20 flex justify-evenly items-center space-x-20">
+                    <div className="w-[400px] space-y-5">
+                        <h3 className="text-3xl font-extrabold font-sans w-[450px] text-gray-200">
+                                New to Cashout? We've got you covered 
+                        </h3>
+
+                        <p className="text-sm pb-6 text-white">
+                            Save and earn interest on your dollar stable cryptocurrency with bundle vault - No risk involved 
+                        </p>
+                            
+                        <a href="#" className="border border-blue-800 px-8 py-2 bg-blue-800 text-gray-300 hover:text-white text-sm rounded-md">
+                            Learn about Cashout
+                        </a>
+                    </div>
+
+                    <div>
+                        <Image src="/image/fourth(2).png" alt="" width="200" height="200" />
+                    </div>
+                </div>
+            </div>
+
+            <div className="p-48">
+                <div className="flex flex-col space-y-3 justify-center items-center bg-slate-100 px-32 py-20 border shadow-2xl shadow-blue-200 rounded-tr-[100px] rounded-bl-[100px]">
+                    <h3 className="text-3xl font-extrabold font-sans text-blue-800">
+                        Download the Cashout App
+                    </h3>
+
+                    <p className="text-center text-sm text-gray-700">
+                        Join about 2 million people who buy sell, pay & get paid with ZekeApp. No commission on any payments you receive.
+                    </p>
+
+                    <div className="flex space-x-2 justify-center items-center">
+                        <div className="flex items-center space-x-3 border px-5 py-2 text-sm hover:rounded-br-2xl rounded-bl-2xl rounded-tl-2xl rounded-tr-2xl border-blue-300 bg-white cursor-pointer">
+                            <IoLogoGooglePlaystore className="text-2xl text-[#27837e]"/>
+
+                            <div>
+                                <p>
+                                    Get it on
+                                </p>
+                                <span>
+                                    Google Play
+                                </span>
+                            </div>
+                        </div>
+
+                        <div className="flex items-center space-x-3 border px-5 py-2 text-sm hover:rounded-br-2xl rounded-bl-2xl rounded-tl-2xl rounded-tr-2xl border-blue-300 bg-white cursor-pointer">
+                            <IoLogoApple className="text-2xl"/>
+
+                            <div>
+                                <p>
+                                    Download on the 
+                                </p>
+                                <span>
+                                    App Store
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* footer */}
+            <div>
+                <div>
+                    <div>
+
+                    </div>
+
+                    <div>
+
+                    </div>
+
+                    <div>
+
                     </div>
                 </div>
             </div>
