78579dfbcf8721abe117e725e62a72e75d84438b
bammytoye <bamigbalatoyese@gmail.com>
2024-08-26 11:13:07 +0100

    responsiveness

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,56 @@
-import HomePage from '../components/home'
+'use client'
+import { useState } from 'react';
+import HomePage from '../components/home';
+import { MdClose } from "react-icons/md";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 export default function Home() {
+  const [toggle, setToggle] = useState(false);
+  const handleClick = () => setToggle(!toggle);
+
   return (
     <>
-      <div className="fixed top-0 right-0 left-0 py-4 px-32 cursor-pointer bg-gradient-to-r from-blue-200 via-purple-700 to-pink-800 shadow-lg z-50">
+      <div className="fixed top-0 right-0 left-0 py-4 px-8 sm:px-16 lg:px-32 cursor-pointer bg-gradient-to-r from-blue-200 via-purple-700 to-pink-800 shadow-lg z-50">
         <div className="flex justify-between items-center text-white">
           <div>
-            <h2>Zeke App</h2>
+            <h2 className="text-lg">Zeke App</h2>
           </div>
 
-          <div className="flex justify-between space-x-10">
+          <div className="lg:flex justify-between space-x-10 hidden">
             <select className="outline-none bg-transparent cursor-pointer hover:text-red-300">
               <option value="Company">Company</option>
-              <option value="Company">Company One</option>
-              <option value="Company">Company Two</option>
+              <option value="Company One">Company One</option>
+              <option value="Company Two">Company Two</option>
             </select>
 
             <h2 className='hover:text-red-300'>Product</h2>
             <h2 className='hover:text-red-300'>Blog</h2>
           </div>
 
+          <div className="hidden lg:flex space-x-10">
+            <a href="#" className='hover:text-red-300 hover:border-l-2 border-r-2 px-3 rounded-full border-white'>Sign Up</a>
+            <a href="#" className='hover:text-red-300 text-pink-600 border bg-white hover:bg-transparent rounded-full px-5 pt-1'>Login</a>
+          </div>
+
+          <div className="lg:hidden flex items-center">
+            <button onClick={handleClick}>
+              {toggle ? <MdClose /> : <RxHamburgerMenu />}
+            </button>
+          </div>
+        </div>
+
+        <div className={toggle ? "lg:hidden flex flex-col" : "hidden"}>
+          <div className="flex flex-col space-y-2">
+            <select className="outline-none bg-transparent cursor-pointer hover:text-red-300">
+              <option value="Company">Company</option>
+              <option value="Company One">Company One</option>
+              <option value="Company Two">Company Two</option>
+            </select>
+
+            <h2 className='hover:text-red-300'>Product</h2>
+            <h2 className='hover:text-red-300'>Blog</h2>
+          </div>
+          
           <div className="flex space-x-10">
             <a href="#" className='hover:text-red-300 hover:border-l-2 border-r-2 px-3 rounded-full border-white'>Sign Up</a>
             <a href="#" className='hover:text-red-300 text-pink-600 border bg-white hover:bg-transparent rounded-full px-5 pt-1'>Login</a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screen: {
+        'xs': '0px',    // Custom breakpoint for 0px and above
+        'sm': '640px',  // Small screens (640px and above)
+        'md': '768px',  // Medium screens (768px and above)
+        'lg': '1024px', // Large screens (1024px and above)
+        'xl': '1280px', // Extra large screens (1280px and above)
+      }
     },
   },
   plugins: [],
