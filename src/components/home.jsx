ca34200f6f6880f508c9d343d7a5e5d08471c7e7
bammytoye <bamigbalatoyese@gmail.com>
2024-09-04 12:23:03 +0100

    responsiveness

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,70 +8,77 @@ const home = () => {
     return (
         <div>
             {/* first component */}
-            <div className="pt-32 px-32 py-32 cursor-pointer h-[580px] shadow-xl bg-gradient-to-r from-blue-200 via-purple-700 to-pink-800 shadow-gray-300 md:rounded-br-[500px] md:rounded-tl-[300px]">
+            <div className="pt-32 md:px-32 px-8 py-32 cursor-pointer h-[580px] shadow-xl bg-gradient-to-r from-blue-200 via-purple-700 to-pink-800 shadow-gray-300 md:rounded-br-[500px] md:rounded-tl-[300px]">
                 <div className='flex flex-col justify-center items-center space-y-6 text-white'>
-                    <h2 className='font-extrabold text-4xl'>
+                    <h2 className='font-extrabold lg:text-4xl md:text-2xl text-[30px] text-center'>
                         Discover the social side of Cashout
                     </h2>
 
-                    <p className='text-sm text-gray-200'>
+                    <p className='text-sm text-white text-center'>
                         A platform for accepting payment, making payments and paying bills
                     </p>
 
-                    <div className='flex space-x-10'>
+                    <div className='flex flex-col sm:flex-row md:space-x-10 lg:space-x-10 sm:justify-center space-y-3 sm:space-y-0'>
                         <button className='border hover:bg-transparent hover:text-white rounded-sm bg-white px-7 py-1 text-black text-[13px]'>Get Started</button>
                         <button className='border hover:text-black hover:bg-gradient-to-r from-blue-300 via-blue-100 to-blue-800 rounded-sm bg-transparent px-5 py-1 text-[13px]'>Join Our Community</button>
                     </div>
 
-                    <div className="pt-20">
-                        <Image src="/image/Cashout 1.png" alt='HomePix' width='300' height='300'/>
+
+                    <div className="pt-12 md:pt-20 flex justify-center">
+                        <Image
+                            src="/image/Cashout 1.png"
+                            alt='HomePix'
+                            width={300}
+                            height={300}
+                            className="w-1/2 sm:w-1/3 md:w-[300px] lg:w-[300px]"
+                        />
                     </div>
+
                 </div>
             </div>
 
             {/* second component */}
-            <div className='flex justify-center items-center px-48 py-32'>
-                <div className="flex justify-between items-center space-x-10 px-10 py-12 border shadow-2xl shadow-blue-200 rounded-tr-[100px] rounded-bl-[100px]">
-                    <div className='flex flex-col justify-center items-center space-y-1 text-center border-r-2 border-gray-600 px-12'>
-                        <Image src="/image/tech.png" alt='Sign Up' width='50' height='50'/>
+            <div className='flex justify-center items-center px-8 md:px-48 py-32'>
+                <div className="flex flex-col lg:flex-row justify-between items-center space-y-5 sm:space-x-10 px-10 py-12 border shadow-2xl shadow-blue-200 rounded-tr-[100px] rounded-bl-[100px]">
+                    <div className='flex flex-col justify-center items-center space-y-1 text-center border-b-2 py-4 border-gray-600 px-3 md:px-3 sm:border-b-2 lg:border-b-0 xl:border-b-0'>
+                        <Image src="/image/tech.png" alt='Sign Up' width='50' height='50' />
                         <h3 className='font-bold text-gray-600 cursor-pointer'>
                             Sign Up
                         </h3>
-                        
                         <p className='text-sm text-gray-900'>
-                            Create a profile and confirm your identity. Its that simple to begin your crypto journey. Lets go!! 
+                            Create a profile and confirm your identity. It's that simple to begin your crypto journey. Let's go!!
                         </p>
                     </div>
 
-                    <div className='flex flex-col justify-center items-center space-y-1 text-center border-r-2 border-gray-600 px-12'>
-                        <Image src="/image/tech.png" alt='Sign Up' width='50' height='50'/>
+                    <div className='flex flex-col justify-center items-center space-y-1 text-center border-b-2 border-gray-600 py-4 px-3 md:px-3 sm:border-b-2 lg:border-b-0 xl:border-b-0'>
+                        <Image src="/image/tech.png" alt='Save Funds' width='50' height='50' />
                         <h3 className='font-bold text-gray-600 cursor-pointer'>
                             Save Funds
                         </h3>
-                        
                         <p className='text-sm text-gray-900'>
-                            Create a profile and confirm your identity. Its that simple to begin your crypto journey. Lets go!! 
+                            Create a profile and confirm your identity. It's that simple to begin your crypto journey. Let's go!!
                         </p>
                     </div>
 
-                    <div className='flex flex-col justify-center items-center space-y-1 text-center px-10'>
-                        <Image src="/image/tech.png" alt='Sign Up' width='50' height='50'/>
+
+                    <div className='flex flex-col justify-center items-center space-y-1 text-center px-3 md:px-3'>
+                        <Image src="/image/tech.png" alt='Make Payment' width='50' height='50' />
                         <h3 className='font-bold text-gray-600 cursor-pointer'>
                             Make Payment
                         </h3>
-                        
                         <p className='text-sm text-gray-900'>
-                            Create a profile and confirm your identity. Its that simple to begin your crypto journey. Lets go!! 
+                            Create a profile and confirm your identity. It's that simple to begin your crypto journey. Let's go!!
                         </p>
                     </div>
                 </div>
             </div>
+
 
             {/* third component */}
             <div className="py-10">
                 <div className='flex justify-center space-x-32 items-center px-44 py-7'>
                     <div>
-                        <Image src="/image/thrid.png" alt="phone" width="300" height="300"/>
+                        <Image src="/image/thrid.png" alt="phone" width="300" height="300" />
                     </div>
 
                     <div className='flex flex-col items-start space-y-6'>
@@ -92,68 +99,68 @@ const home = () => {
 
             {/* fourth component */}
             <div className="px-32 py-10">
-                    <div className="flex flex-col justify-center items-center space-y-20">
-                        <div className="flex justify-between items-center space-x-10">
-                            <div className="w-[400px] space-y-5">
-                                <h3 className="text-4xl font-extrabold font-sans">
-                                    Start Selling on the Cashout App
-                                </h3>
-
-                                <p className="text-sm text-gray-700 pb-6">
-                                    Save and earn interest on your dollar stable cryptocurrency with bundle vault - No risk involved 
-                                </p>
-
-                                <a href="#" className="border px-8 py-2 bg-blue-800 text-gray-300 hover:text-white text-sm rounded-md">
-                                    Start Now
-                                </a>
-                            </div>
-
-                            <div>
-                                <Image src="/image/fourth.png" alt="" width="300" height="700"/>
-                            </div>
-
-                            <div className="space-y-4">
-                                <div className="flex space-x-3 font-sm text-sm font-sans">
-                                    <TiStarOutline />
-                                    <p>
-                                        Use the easypay to start making profit  
-                                    </p>
-                                </div>
-
-                                <div className="flex space-x-3 font-sm text-sm font-sans">
-                                    <TiStarOutline />
-                                    <p>
-                                        Use the easypay to start making profit  
-                                    </p>
-                                </div>
-
-                                <div className="flex space-x-3 font-sm text-sm font-sans">
-                                    <TiStarOutline />
-                                    <p>
-                                        Use the easypay to start making profit  
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="text-center space-y-4">
+                <div className="flex flex-col justify-center items-center space-y-20">
+                    <div className="flex justify-between items-center space-x-10">
+                        <div className="w-[400px] space-y-5">
                             <h3 className="text-4xl font-extrabold font-sans">
-                                New to Cashout? We've got you covered 
+                                Start Selling on the Cashout App
                             </h3>
 
                             <p className="text-sm text-gray-700 pb-6">
-                                Save and earn interest on your dollar stable cryptocurrency with bundle vault - No risk involved 
+                                Save and earn interest on your dollar stable cryptocurrency with bundle vault - No risk involved
                             </p>
-                            
+
                             <a href="#" className="border px-8 py-2 bg-blue-800 text-gray-300 hover:text-white text-sm rounded-md">
-                                Learn about Cashout
+                                Start Now
                             </a>
                         </div>
 
                         <div>
-                            <Image src="/image/fourth(2).png" alt="" width="300" height="300" />
+                            <Image src="/image/fourth.png" alt="" width="300" height="700" />
+                        </div>
+
+                        <div className="space-y-4">
+                            <div className="flex space-x-3 font-sm text-sm font-sans">
+                                <TiStarOutline />
+                                <p>
+                                    Use the easypay to start making profit
+                                </p>
+                            </div>
+
+                            <div className="flex space-x-3 font-sm text-sm font-sans">
+                                <TiStarOutline />
+                                <p>
+                                    Use the easypay to start making profit
+                                </p>
+                            </div>
+
+                            <div className="flex space-x-3 font-sm text-sm font-sans">
+                                <TiStarOutline />
+                                <p>
+                                    Use the easypay to start making profit
+                                </p>
+                            </div>
                         </div>
                     </div>
+
+                    <div className="text-center space-y-4">
+                        <h3 className="text-4xl font-extrabold font-sans">
+                            New to Cashout? We've got you covered
+                        </h3>
+
+                        <p className="text-sm text-gray-700 pb-6">
+                            Save and earn interest on your dollar stable cryptocurrency with bundle vault - No risk involved
+                        </p>
+
+                        <a href="#" className="border px-8 py-2 bg-blue-800 text-gray-300 hover:text-white text-sm rounded-md">
+                            Learn about Cashout
+                        </a>
+                    </div>
+
+                    <div>
+                        <Image src="/image/fourth(2).png" alt="" width="300" height="300" />
+                    </div>
+                </div>
             </div>
 
             {/* fifth component */}
@@ -161,13 +168,13 @@ const home = () => {
                 <div className="px-52 py-20 flex justify-evenly items-center space-x-20">
                     <div className="w-[400px] space-y-5">
                         <h3 className="text-3xl font-extrabold font-sans w-[450px] text-gray-200">
-                                New to Cashout? We've got you covered 
+                            New to Cashout? We've got you covered
                         </h3>
 
                         <p className="text-sm pb-6 text-white">
-                            Save and earn interest on your dollar stable cryptocurrency with bundle vault - No risk involved 
+                            Save and earn interest on your dollar stable cryptocurrency with bundle vault - No risk involved
                         </p>
-                            
+
                         <a href="#" className="border border-blue-800 px-8 py-2 bg-blue-800 text-gray-300 hover:text-white text-sm rounded-md">
                             Learn about Cashout
                         </a>
@@ -178,7 +185,7 @@ const home = () => {
                     </div>
                 </div>
             </div>
-    
+
             {/* sixth component */}
             <div className="p-48">
                 <div className="flex flex-col space-y-3 justify-center items-center bg-slate-100 px-32 py-20 border shadow-2xl shadow-blue-200 rounded-tr-[100px] rounded-bl-[100px]">
@@ -192,7 +199,7 @@ const home = () => {
 
                     <div className="flex space-x-2 justify-center items-center">
                         <div className="flex items-center space-x-3 border px-5 py-2 text-sm hover:rounded-br-2xl rounded-bl-2xl rounded-tl-2xl rounded-tr-2xl border-blue-300 bg-white cursor-pointer">
-                            <IoLogoGooglePlaystore className="text-2xl text-[#27837e]"/>
+                            <IoLogoGooglePlaystore className="text-2xl text-[#27837e]" />
 
                             <div>
                                 <p>
@@ -205,11 +212,11 @@ const home = () => {
                         </div>
 
                         <div className="flex items-center space-x-3 border px-5 py-2 text-sm hover:rounded-br-2xl rounded-bl-2xl rounded-tl-2xl rounded-tr-2xl border-blue-300 bg-white cursor-pointer">
-                            <IoLogoApple className="text-2xl"/>
+                            <IoLogoApple className="text-2xl" />
 
                             <div>
                                 <p>
-                                    Download on the 
+                                    Download on the
                                 </p>
                                 <span>
                                     App Store
@@ -234,7 +241,7 @@ const home = () => {
 
                         <div className="flex space-x-2 items-center">
                             <div className="flex items-center space-x-3 border px-5 py-2 text-sm hover:rounded-br-2xl rounded-bl-2xl rounded-tl-2xl rounded-tr-2xl border-white bg-transparent text-white cursor-pointer">
-                                <IoLogoGooglePlaystore className="text-2xl text-[#27837e]"/>
+                                <IoLogoGooglePlaystore className="text-2xl text-[#27837e]" />
 
                                 <div>
                                     <p>
@@ -247,11 +254,11 @@ const home = () => {
                             </div>
 
                             <div className="flex items-center space-x-3 border px-5 py-2 text-sm hover:rounded-br-2xl rounded-bl-2xl rounded-tl-2xl rounded-tr-2xl border-white bg-transparent text-white cursor-pointer">
-                                <IoLogoApple className="text-2xl"/>
+                                <IoLogoApple className="text-2xl" />
 
                                 <div>
                                     <p>
-                                        Download on the 
+                                        Download on the
                                     </p>
                                     <span>
                                         App Store
@@ -332,14 +339,14 @@ const home = () => {
                     </p>
 
                     <p>
-                        Terms of Service | Privacy Policy 
+                        Terms of Service | Privacy Policy
                     </p>
 
                     <div className="flex space-x-4">
-                        <FaFacebook  />
-                        <FaTwitter  />
-                        <FaLinkedinIn  />
-                        <FaInstagram  />
+                        <FaFacebook />
+                        <FaTwitter />
+                        <FaLinkedinIn />
+                        <FaInstagram />
                     </div>
                 </div>
             </div>
